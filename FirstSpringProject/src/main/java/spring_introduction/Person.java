fe0e5172204bd605209c309d79c80498e5bb73bc
Aleksey Petrik <alexey.petric@gmail.com>
2021-10-20 19:22:08 +1000

    applicationContext.xml add info comments and input parameters. applicationContextTestScopeSingleton.xml test scope="singleton". applicationContextTestScopePrototype.xml test scope="prototype". class Person add vars and gets/sets. class Test add new test. myApp.properties data file.

--- FirstSpringProject/src/main/java/spring_introduction/Person.java
+++ FirstSpringProject/src/main/java/spring_introduction/Person.java
@@ -2,11 +2,30 @@ package spring_introduction;
 
 public class Person {
     private Pet pet;
+    private String surname;
+    private int age;
 
     public Person(Pet pet) {
         this.pet = pet;
     }
+
     public Person() {
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public void setPet(Pet pet) {
--- FirstSpringProject/src/main/java/spring_introduction/Test.java
+++ FirstSpringProject/src/main/java/spring_introduction/Test.java
@@ -9,9 +9,12 @@ public class Test {
         test2();
         test3();
         test4();
+        test5();
+        test6();
     }
 
     public static void test() {
+        System.out.println("Test - 1");
         //Создание бинов с помощью xml
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Pet pet = context.getBean("myPet", Pet.class);
@@ -21,6 +24,7 @@ public class Test {
     }
 
     public static void test2() {
+        System.out.println("Test - 2");
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Pet pet = context.getBean("myPet", Pet.class);
         Person person = new Person(pet);
@@ -29,6 +33,7 @@ public class Test {
     }
 
     public static void test3() {
+        System.out.println("Test - 3");
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Person person = context.getBean("myPerson", Person.class);
         person.callMyPet();
@@ -36,10 +41,34 @@ public class Test {
     }
 
     public static void test4() {
+        System.out.println("Test - 4");
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Person person = context.getBean("myPersonSet", Person.class);
         person.callMyPet();
+        System.out.println("Name owner - " + person.getSurname());
+        System.out.println("Age owner - " + person.getAge());
         context.close();
     }
 
+    public static void test5() {
+        System.out.println("Test(singleton) - 5");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContextTestScopeSingleton.xml");
+        Pet dog = context.getBean("myPet", Dog.class);
+        Pet dogSecond = context.getBean("myPet", Dog.class);
+        //Возвращает один и тот же объект
+        System.out.println(dog);
+        System.out.println(dogSecond);
+        context.close();
+    }
+
+    public static void test6() {
+        System.out.println("Test(prototype) - 6");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContextTestScopePrototype.xml");
+        Pet dog = context.getBean("myPet", Dog.class);
+        Pet dogSecond = context.getBean("myPet", Dog.class);
+        //Возвращаются разные объекты
+        System.out.println(dog);
+        System.out.println(dogSecond);
+        context.close();
+    }
 }
