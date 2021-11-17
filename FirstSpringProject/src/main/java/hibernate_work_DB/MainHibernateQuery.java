95d942ab62db062240c1d1a4a9654f69d71c3d66
Aleksey Petrik <alexey.petric@gmail.com>
2021-11-17 21:39:12 +1000

    classes Employee and Detail it tables in database, tables joins one-to-one. class MainHibernateQuery test query(get/delete/save) for tables Employees and Details.

---
+++ FirstSpringProject/src/main/java/hibernate_work_DB/MainHibernateQuery.java
@@ -0,0 +1,50 @@
+package hibernate_work_DB;
+
+import hibernate_work_DB.entity.Detail;
+import hibernate_work_DB.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class MainHibernateQuery {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")//начитывает конфигурацию для работы с БД
+                .addAnnotatedClass(Employee.class)//читает аннотации
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();//создает объект
+        Session session = null;
+        try {
+            //язык запросов HQL
+            //factory можем переиспользовать
+            session = factory.getCurrentSession();//создание сессии для работы с БД, только для одного действия
+
+            //создаем объект рабочий
+            Employee employee = new Employee("Alex", "Petrov", "IT", 3000.00);
+            //Создаем объект доп. информации
+            Detail detail = new Detail("KnA", "+79998886677", "alex@gmail.com");
+            //Рабочему присваиваем доп. информацию
+            employee.setEmployeeDetails(detail);
+
+            //создаем объект рабочий
+            Employee employee2 = new Employee("Oleg", "Smirnov", "Sale", 2000.00);
+            //Создаем объект доп. информации
+            Detail detail2 = new Detail("KnA", "+76668885571", "oleg@gmail.com");
+            //Рабочему присваиваем доп. информацию
+            employee2.setEmployeeDetails(detail2);
+
+            session.beginTransaction();//открытие транзакции, которую необходимо закрыть, принять либо откатить изменения
+            /* Удаление
+            Employee employeeDelete = session.get(Employee.class, 1);
+            session.delete(employeeDelete);
+             */
+            session.save(employee);
+            session.save(employee2);
+            Employee employeeShow = session.get(Employee.class, 2);
+            System.out.println(employeeShow);
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+            factory.close();//закрытие фабрики сессий, при любом исходе так как finally
+        }
+    }
+}
---
+++ FirstSpringProject/src/main/java/hibernate_work_DB/entity/Employee.java
@@ -0,0 +1,98 @@
+package hibernate_work_DB.entity;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees_course")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "department")
+    private String department;
+    @Column(name = "salary")
+    private Double salary;
+    /* foreign key поле не указываем, при каскадном обновлении заполнится автоматически
+        @Column(name = "details_id")
+        private int detailsId;
+     */
+    @OneToOne(cascade = CascadeType.ALL)//Связь между таблица один-к-одному, с каскадным типом подчинения
+    @JoinColumn(name = "details_id")//Аннотация объединения таблиц по столбцу "details_id"
+    private Detail employeeDetail;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String department, Double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Detail getEmployeeDetails() {
+        return employeeDetail;
+    }
+
+    public void setEmployeeDetails(Detail employeeDetail) {
+        this.employeeDetail = employeeDetail;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                ", employeeDetail=" + employeeDetail +
+                '}';
+    }
+}
