34d99ec873adee5f6c77087b9a9e1133bdfea09f
Aleksey Petrik <alexey.petric@gmail.com>
2021-11-20 21:10:14 +1000

    refactoring.

--- FirstSpringProject/src/main/java/hibernate_one_to_many_uni/MainHibernateQuery.java
+++ FirstSpringProject/src/main/java/hibernate_one_to_many_uni/MainHibernateQuery.java
@@ -8,15 +8,13 @@ import org.hibernate.cfg.Configuration;
 
 public class MainHibernateQuery {
     public static void main(String[] args) {
-        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")//начитывает конфигурацию для работы с БД
+        try (SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")//начитывает конфигурацию для работы с БД
                 .addAnnotatedClass(Employee.class)//читает аннотации
-                .addAnnotatedClass(Department.class)
-                .buildSessionFactory();//создает объект
-        Session session = null;
-        try {
+                .addAnnotatedClass(Department.class)//читает аннотации
+                .buildSessionFactory();
+             Session session = factory.getCurrentSession()) {//создание сессии для работы с БД, только для одного действия
             //язык запросов HQL
             //factory можем переиспользовать
-            session = factory.getCurrentSession();//создание сессии для работы с БД, только для одного действия
             session.beginTransaction();//открытие транзакции, которую необходимо закрыть, принять либо откатить изменения
             /*
             Department department = new Department("HR", 500.00, 1500.00);
@@ -24,18 +22,13 @@ public class MainHibernateQuery {
             Employee employee2 = new Employee("Vladimir", "Suvorov", 2500.00);
             department.addEmployeeTODepartment(employee);
             department.addEmployeeTODepartment(employee2);
-
             session.save(department);
              */
-
-
             Department department = session.get(Department.class, 2);
             department.getEmployees().forEach(System.out::println);
-
             session.getTransaction().commit();
-        } finally {
-            session.close();
-            factory.close();//закрытие фабрики сессий, при любом исходе так как finally
         }
+        //session.close();
+        //закрытие фабрики сессий, при любом исходе так как finally
     }
 }
